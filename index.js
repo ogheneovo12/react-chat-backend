7f566941c1f291ced6191e7a215db7caf83ef472
ogheneovo12 <ukuanovweogheneovo@gmail.com>
2020-04-05 05:51:01 +0100

    set up socket on server side

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
-const express = require('express')
-const socketIo = require('socket.io')
-const http = require('http')
+const express = require("express");
+const socketIo = require("socket.io");
+const http = require("http");
 const app = express();
 const PORT = process.env.port || 5000;
 const server = http.createServer(app);
-const io = socketIo(server)
+const io = socketIo(server);
+const router = require("./router");
+app.use(router);
 
-server.listen(PORT,()=>console.log(`server is running on port ${PORT}`))
+//listen to io connection event
+io.on("connection",socket =>{
+    console.log("we've got a new user")
+    socket.on("disconnect",()=>{
+        console.log("user left chatroom")
+    })
+})
+
+
+
+server.listen(PORT, () => console.log(`server is running on port ${PORT}`));
